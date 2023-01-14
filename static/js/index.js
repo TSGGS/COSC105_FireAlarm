5d18f21d04a5013f1b48817d3324b710f63d414c
TSGGS <sagioatscie@gmail.com>
2023-01-14 11:03:58 +0800

    added building

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,9 @@
 let fire = new Fire();
 let fire_alarm = new FireAlarm();
 
+let selected_room = "";
+let alarm_threshold = "";
+
 function checkFireRequirement() {
     let fire_value = fire.getFireValue()
     let alarm_threshold = fire_alarm.getThresholdValue()
@@ -21,6 +24,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     fire.setThresholdValue(response['light'], response['heat'], response['smoke']);
     fire_alarm.setThresholdValue(response['light'], response['heat'], response['smoke']);
+    alarm_threshold = fire_alarm.getThresholdValue()
 });
 
 document.getElementById('fire-control-light').addEventListener('input', () => {
@@ -41,11 +45,29 @@ document.getElementById('fire-control-heat').addEventListener('input', () => {
 
 document.getElementById('fire-control-smoke').addEventListener('input', () => {
     let smoke_value = document.getElementById('fire-control-smoke').value;
-    document.getElementById('smoke-control-value').innerText = parseFloat(smoke_value).toFixed(2);
-    fire.setSmokeValue(parseFloat(smoke_value).toFixed(2));
+    let parsed_smoke_value = parseFloat(smoke_value).toFixed(2);
+    document.getElementById('smoke-control-value').innerText = parsed_smoke_value;
+    fire.setSmokeValue(parsed_smoke_value);
+
+    let win = document.querySelector(`#${selected_room}`);
+    let smoke = win.querySelector(".smoke");
+    if(parsed_smoke_value >= alarm_threshold["smoke"]) {
+        smoke.classList.remove("hidden");
+        win.append(smoke);
+    } else {
+        smoke.classList.add("hidden");
+    }
 
     checkFireRequirement();
 });
 
-// CONNECT FIRE AND FIRE ALARM
-fire_alarm.addFire(fire);
+// ADD EVENT LISTENER TO WINDOW DIVS
+let windows = document.querySelectorAll(".window");
+
+windows.forEach(window => {
+    window.addEventListener('click', () => {
+        selected_room = window.id;
+        document.querySelector("#selected-room").innerText = selected_room;
+
+    });
+});
